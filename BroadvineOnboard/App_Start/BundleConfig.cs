2278116178dd7afa3ece6f0929934ebf704b9aed
Kurt Lippert <klippert@broadvine.com>
2016-04-29 10:31:48 -0400

    removed kendo cuz I want to add it through bower. Also added bower (which includes it .json file)

--- BroadvineOnboard/App_Start/BundleConfig.cs
+++ BroadvineOnboard/App_Start/BundleConfig.cs
@@ -22,12 +22,12 @@ namespace BroadvineOnboard
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/kendo").Include(
-                "~/Scripts/kendo/kendo.ui.core.min.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/kendo").Include(
+            //    "~/Scripts/kendo/kendo.ui.core.min.js"));
 
-            bundles.Add(new StyleBundle("~/Content/kendo").Include(
-                "~/Content/kendo/web/kendo.common.core.min.css",
-                "~/Content/kendo/web/kendo.bootstrap.min.css"));
+            //bundles.Add(new StyleBundle("~/Content/kendo").Include(
+            //    "~/Content/kendo/web/kendo.common.core.min.css",
+            //    "~/Content/kendo/web/kendo.bootstrap.min.css"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
