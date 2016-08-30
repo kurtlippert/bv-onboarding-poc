85231cad3b926cbd62b78a445870fb42fd26b4ea
Kurt Lippert <klippert@broadvine.com>
2016-08-30 12:57:30 -0400

    BV-841 Copied format of Department controller methods to Property for consistency. Controllers are roughly equivalent except for the models they pass around

--- BroadvineOnboard/Controllers/PropertyController.cs
+++ BroadvineOnboard/Controllers/PropertyController.cs
@@ -26,6 +26,45 @@ namespace BroadvineOnboard.Controllers
             if (!string.IsNullOrEmpty(msg)) ViewBag.Error = msg;
 
             return View(db.Properties.ToList());
+        }                                                 
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+            string failedMessage = "";
+
+            if (file != null && file.ContentLength > 0)
+            {
+                string filename = file.FileName.ToLower();
+                if (filename.EndsWith("xls") || filename.EndsWith("xlsx"))
+                {
+                    try
+                    {
+                        var folderFilename = Path.Combine(Server.MapPath(System.Web.Configuration.WebConfigurationManager.AppSettings["UploadFolder"]), Path.GetFileName(file.FileName));
+                        file.SaveAs(folderFilename);
+
+                        ExcelSpreadSheet excel = new ExcelSpreadSheet(folderFilename, "Property");
+                        if (excel.WorkSheetNames.Count() == 1) excel.SelectedWorksheet = excel.WorkSheetNames.First().ToString();
+                        Helpers.CurrentClientUpload = excel;
+
+                        return RedirectToAction("WorkSheet");
+
+                    }
+                    catch (Exception ex)
+                    {
+                        failedMessage = ex.Message;
+                    }
+
+                }
+                else failedMessage = "Only valid excel spreadsheets are excepted";
+
+            }
+            else failedMessage = "The file contained no data.";
+
+            if (!string.IsNullOrEmpty(failedMessage))
+                return RedirectToAction("Index", "Property", new { msg = failedMessage });
+
+            return RedirectToAction("Index");
         }
 
         // GET: Property/Details/5
@@ -129,17 +168,56 @@ namespace BroadvineOnboard.Controllers
             return RedirectToAction("Index");
         }
 
-        protected override void Dispose(bool disposing)
+        public ActionResult WorkSheet()
         {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
+            var result = (from w in Helpers.CurrentClientUpload.WorkSheetNames select new SelectListItem() { Text = w, Value = w }).ToList();
+            ViewBag.WorkSheetNames = result;
+            return View();
         }
 
+        [HttpPost]
+        public ActionResult WorkSheet(FormCollection values)
+        {
+            ExcelSpreadSheet s = Helpers.CurrentClientUpload;
+            s.SelectedWorksheet = values["worksheetName"];
+            s.RowStart = int.Parse(values["rowNumber"]);
+            Helpers.CurrentClientUpload = s;
 
-        //  Excel spreadsheet stuff ###########################################################################################################################################################################################
+            return RedirectToAction("Import");
+        }
+
+        //  Import 
+        public ActionResult Import()
+        {
+            string[] columnNames = Helpers.CurrentClientUpload.Columns.ToArray();
+            ViewBag.AreaID = new SelectList(db.Areas, "AreaID", "Name");
+            ViewBag.CompanyID = new SelectList(db.Companies, "CompanyID", "Name");
+
+            Array.IndexOf(columnNames, "");
+            List<SelectListItem> list = new List<SelectListItem>();
+            for (int i = 0; i < columnNames.Length; i++)
+            {
+                list.Add(new SelectListItem { Text = columnNames[i], Value = i.ToString() });
+            }
+
+            ViewBag.Columns = list;
+
+            //Collection
+            return View(new PropertyViewModel());
+        }
+
+        public JsonResult GetPropertiesFromClient(int Id)
+        {
+            string[] columnNames = Helpers.CurrentClientUpload.Columns.ToArray();
+            List<SelectListItem> list = new List<SelectListItem>();
+            list.Add(new SelectListItem { Text = "(Don't Map)", Value = "-1" });
+            for (int i = 0; i < columnNames.Length; i++)
+            {
+                list.Add(new SelectListItem { Text = columnNames[i], Value = i.ToString() });
+            }
+
+            return Json(list, JsonRequestBehavior.AllowGet);
+        }
 
         [HttpPost]
         public ActionResult Sheet(FormCollection values)
@@ -188,173 +266,16 @@ namespace BroadvineOnboard.Controllers
 
             db.SaveChanges();
 
-
-            return RedirectToAction("Index");
-
-        }
-
-        [HttpPost]
-        public ActionResult Index(HttpPostedFileBase file)
-        {
-            string failedMessage = "";
-
-            if (file != null && file.ContentLength > 0)
-            {
-                string filename = file.FileName.ToLower();
-                if (filename.EndsWith("xls") || filename.EndsWith("xlsx"))
-                {
-                    try
-                    {
-                        var folderFilename = Path.Combine(Server.MapPath(System.Web.Configuration.WebConfigurationManager.AppSettings["UploadFolder"]), Path.GetFileName(file.FileName));
-                        file.SaveAs(folderFilename);
-
-                        ExcelSpreadSheet excel = new ExcelSpreadSheet(folderFilename, "Property");
-                        if (excel.WorkSheetNames.Count() == 1) excel.SelectedWorksheet = excel.WorkSheetNames.First().ToString();
-                        Helpers.CurrentClientUpload = excel;
-
-                        if (excel.WorkSheetNames.Count() == 1) return RedirectToAction("Import");
-                        else return RedirectToAction("WorkSheet");
-
-                    }
-                    catch (Exception ex)
-                    {
-                        failedMessage = ex.Message;
-                    }
-
-                }
-                else failedMessage = "Only valid excel spreadsheets are excepted";
-
-            }
-            else failedMessage = "The file contained no data.";
-
-            if (!string.IsNullOrEmpty(failedMessage))
-                return RedirectToAction("Index", "Property", new { msg = failedMessage });
-
             return RedirectToAction("Index");
         }
 
-        public ActionResult WorkSheet()
+        protected override void Dispose(bool disposing)
         {
-            var result = (from w in Helpers.CurrentClientUpload.WorkSheetNames select new SelectListItem() { Text = w, Value = w }).ToList();
-            ViewBag.WorkSheetNames = result;
-            return View();
-        }
-
-        //[HttpPost]
-        //public ActionResult Sheet(PropertyViewModel viewModel)
-        //{
-        //    //Helpers.CurrentClientUpload.Columns.
-        //    var data = Helpers.CurrentClientUpload.Rows;
-
-        //    int nameCol = int.Parse(viewModel.Name);
-        //    int codeCol = int.Parse(viewModel.Code);
-        //    int address1Col = int.Parse(viewModel.Address1);
-        //    int address2Col = int.Parse(viewModel.Address2);
-        //    int cityCol = int.Parse(viewModel.City);
-        //    int stateCol = int.Parse(viewModel.State);
-        //    int postalCodeCol = int.Parse(viewModel.PostalCode);
-        //    int brandCodeCol = int.Parse(viewModel.BrandCode);
-        //    int smithTravelCodeCol = int.Parse(viewModel.SmithTravelCode);
-        //    int totalRoomsCol = int.Parse(viewModel.TotalRooms);
-        //    int address1Col = int.Parse(viewModel.Address1);
-
-        //    List<Property> properties = new List<Property>();
-        //    foreach (var row in data)
-        //    {
-        //        string Name = row[int.Parse(viewModel.Name)].Value.ToString().Trim();
-        //        string Code = row[1].Value.ToString().Trim();
-        //        string Address1 = row[2].Value.ToString().Trim();
-        //        string Address2 = row[3].Value.ToString().Trim();
-        //        string City = row[4].Value.ToString().Trim();
-        //        string State = row[5].Value.ToString().Trim();
-        //        string PostalCode = row[6].Value.ToString().Trim();
-        //        string BrandCode = row[7].Value.ToString().Trim();
-        //        string SmithTravelCode = row[8].Value.ToString().Trim();
-        //        int TotalRooms = row[9].Value;
-        //        string URL = row[10].Value.ToString().Trim();
-        //        string Phone = row[11].Value.ToString().Trim();
-        //        string Email = row[12].Value.ToString().Trim();
-        //        bool FoodAndBeverage = (bool)row[13].Value;
-        //        Enums.PropertyServiceType ServiceType = (Enums.PropertyServiceType)row[14].Value;
-        //        Enums.PropertyStatus StatusType = (Enums.PropertyStatus)row[15].Value;
-        //        Enums.PropertyRelationship RelationshipType = (Enums.PropertyRelationship)row[16].Value;
-        //        Enums.PropertyMaturity MaturityType = (Enums.PropertyMaturity)row[17].Value;
-        //        Enums.PropertyCalendar CalendarType = (Enums.PropertyCalendar)row[18].Value;
-        //        Company Company = (Company)row[19].Value;
-        //        Area Area = (Area)row[20].Value;
-
-        //        properties.Add(new Property
-        //        {
-        //            Name = row[0].Value.ToString().Trim(),
-        //            Code = row[1].Value.ToString().Trim(),
-        //            Address1 = row[2].Value.ToString().Trim(),
-        //            Address2 = row[3].Value.ToString().Trim(),
-        //            City = row[4].Value.ToString().Trim(),
-        //            State = row[5].Value.ToString().Trim(),
-        //            PostalCode = row[6].Value.ToString().Trim(),
-        //            BrandCode = row[7].Value.ToString().Trim(),
-        //            SmithTravelCode = row[8].Value.ToString().Trim(),
-        //            TotalRooms = (int)row[9].Value,
-        //            URL = row[10].Value.ToString().Trim(),
-        //            Phone = row[11].Value.ToString().Trim(),
-        //            Email = row[12].Value.ToString().Trim(),
-        //            FoodAndBeverage = (bool)row[13].Value,
-        //            ServiceType = (Enums.PropertyServiceType)row[14].Value,
-        //            StatusType = (Enums.PropertyStatus)row[15].Value,
-        //            RelationshipType = (Enums.PropertyRelationship)row[16].Value,
-        //            MaturityType = (Enums.PropertyMaturity)row[17].Value,
-        //            CalendarType = (Enums.PropertyCalendar)row[18].Value,
-        //            Company = (Company)row[19].Value,
-        //            Area = (Area)row[20].Value
-        //        });            
-        //    }
-
-        //    return View("Index", properties.AsEnumerable());
-        //} 
-
-        [HttpPost]
-        public ActionResult WorkSheet(string worksheetName)
-        {
-            ExcelSpreadSheet s = Helpers.CurrentClientUpload;
-            s.SelectedWorksheet = worksheetName;
-            Helpers.CurrentClientUpload = s;
-
-            return RedirectToAction("Import");
-        }
-
-        //  Import 
-        public ActionResult Import()
-        {
-            string[] columnNames = Helpers.CurrentClientUpload.Columns.ToArray();
-            ViewBag.AreaID = new SelectList(db.Areas, "AreaID", "Name");
-            ViewBag.CompanyID = new SelectList(db.Companies, "CompanyID", "Name");
-
-            Array.IndexOf(columnNames, "");
-            //List<SelectListItem> list = columnNames.Select(x => new SelectListItem { Text = x, Value = columnNames. }).ToList();
-            List<SelectListItem> list = new List<SelectListItem>();
-            for (int i = 0; i < columnNames.Length; i++)
+            if (disposing)
             {
-                list.Add(new SelectListItem { Text = columnNames[i], Value = i.ToString() });
+                db.Dispose();
             }
-
-            ViewBag.Columns = list;
-
-            //Collection
-            return View(new PropertyViewModel());
-        }
-
-        public JsonResult GetPropertiesFromClient(int Id)
-        {
-            string[] columnNames = Helpers.CurrentClientUpload.Columns.ToArray();
-            //List<SelectListItem> list = columnNames.Select(x => new SelectListItem { Text = x, Value = x }).ToList();
-            List<SelectListItem> list = new List<SelectListItem>();
-            list.Add(new SelectListItem { Text = "(Don't Map)", Value = "-1" });
-            for (int i = 0; i < columnNames.Length; i++)
-            {
-                list.Add(new SelectListItem { Text = columnNames[i], Value = i.ToString() });
-            }
-
-            return Json(list, JsonRequestBehavior.AllowGet);
+            base.Dispose(disposing);
         }
     }
 }
