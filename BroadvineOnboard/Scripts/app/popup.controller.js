fc3742765303a80ad0f53eb9c7b2be3ac5b312ad
Kurt Lippert <klippert@broadvine.com>
2016-09-02 12:36:07 -0400

    BV-899 Moved js out of views into dedicated .js files (following John Papa's styles guide for file structure and whotnot). Added Action Url for the wizard content to 'Settings.cshtml' file. Restructed/added new files to _Layout master view

---
+++ BroadvineOnboard/Scripts/app/popup.controller.js
@@ -0,0 +1,65 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('ob')
+        .controller('PopupController', PopupController)
+
+    //PopupController.$inject = ['']
+
+    function PopupController() {
+        var vm = this;
+        vm.file = {};
+
+        vm.fileChanged = function (element) {
+            vm.file = element.files[0];
+        }
+
+        vm.options = {
+            title: "title",
+            modal: true,
+            width: "700",
+            height: "500",
+            resizable: false,
+            visible: false
+        };
+
+        vm.show = function () {
+            var data = new FormData();
+            data.append("file", vm.file);
+
+            var opts = {
+                url: Ob.Urls.WizardProperty,
+                data: data,
+                cache: false,
+                contentType: false,
+                processData: false,
+                type: 'POST',
+                success: function (callback) {
+                    if (!callback.startsWith("<!DOCTYPE")) {
+                        vm.popupWizard.content(callback);
+                        vm.popupWizard.center();
+                        vm.popupWizard.open();
+                    }
+                    else {
+                        if (!window.location.href.endsWith("?msg=The%20file%20contained%20no%20data.")) {
+                            window.location.href = window.location.href + "?msg=The%20file%20contained%20no%20data.";
+                        }
+                    }
+                }
+            };
+
+            if (data.fake) {
+                // Make sure no text encoding stuff is done by xhr
+                opts.xhr = function () { var xhr = $.ajaxSettings.xhr(); xhr.send = xhr.sendAsBinary; return xhr; }
+                opts.contentType = "multipart/form-data; boundary=" + data.boundary;
+                opts.data = data.toString();
+            }
+
+            vm.popupWizard.refresh(opts);
+            //vm.popupWizard.center();
+            //vm.popupWizard.open();
+        };
+    }
+
+})();
---
+++ BroadvineOnboard/Scripts/app/worksheet.controller.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module("ob")
+        .controller("WorksheetController", WorksheetController)
+
+    function WorksheetController() {
+        var vm = this;
+        var defaultValue = 1;
+
+        vm.value = defaultValue;
+        vm.options = {
+            min: "1",
+            max: "100",
+            format: "n0"
+        };
+
+        vm.onBlur = function () {
+            if (vm.value === null) {
+                vm.value = defaultValue;
+            }
+        };
+    }
+
+})();
